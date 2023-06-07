ac8aa5bf2830ba91031ba29911db709c2ccd08db
Dogaru Simona <simona25dogaru@gmail.com>
2023-06-07 16:18:33 +0300

    logare doctor

--- HospitalPlanner/src/main/java/Interfata/LogareDoctor.java
+++ HospitalPlanner/src/main/java/Interfata/LogareDoctor.java
@@ -1,12 +1,18 @@
 package Interfata;
 
+import Utilizatori.Doctor;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class LogareDoctor extends JFrame {
-    private JTextField textField1;
-    private JPasswordField passwordField1;
+    private JTextField txtUser;
+    private JPasswordField txtParola;
     private JButton btnInapoi;
     private JButton logareButton;
     private JPanel LogareDoctorPanel;
@@ -17,6 +23,35 @@ public class LogareDoctor extends JFrame {
         public void actionPerformed(ActionEvent e) {
             /// aici se face autentificarea
             /// se initializeaza Doctorul
+
+            EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("HospitalPlanner");
+            EntityManager entityManager = entityManagerFactory.createEntityManager();
+            String username = txtUser.getText();
+            String password = txtParola.getText();
+
+
+            TypedQuery<Doctor> query = entityManager.createNamedQuery("Doctor.findByUsernameAndPassword", Doctor.class);
+            query.setParameter("username", username);
+            query.setParameter("password", password);
+
+            Doctor doctorLogare = query.getSingleResult();
+            if(doctorLogare != null){
+                if (doctorLogare instanceof Doctor){
+                    System.out.println(doctorLogare);
+                    PaginaDoctor paginaDoctor= new PaginaDoctor(doctorLogare);
+                    paginaDoctor.setContentPane(paginaDoctor.getPaginaDoctorPanel());
+                    paginaDoctor.setTitle("Doctor - Pagina Mea");
+                    //incercare.setSize(300,400);
+                    paginaDoctor.setBounds(600,200,600,400);
+                    paginaDoctor.setVisible(true);
+                    //paginaDoctor.setLabelNumePrenume(txtNume.getText(),txtPrenume.getText());
+                    paginaDoctor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    dispose();
+                }
+
+            }
+
+
 
 
 //            PaginaDoctor paginaDoctor= new PaginaDoctor();
--- HospitalPlanner/src/main/java/Main.java
+++ HospitalPlanner/src/main/java/Main.java
@@ -10,7 +10,7 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-        Doctor John = new Doctor("Johnul","Johnul",  "2001,12,4",  "numar", "username", "parola","Spiridon");
+        Doctor John = new Doctor("Johnul","Johnul",  "2001,12,4",  "usernameJhon", "parolaJhon", "0755555555","Spiridon");
         Pacient Ion = new Pacient("Ion","Ionul","2001,12,4", "adresa", "username","parola", "teelfon");
         ///Doctor Mary = new Doctor("Johnul","Johnul",  new Date(2001,12,4),  "numar", "username", "parola","numar","adresa Spital");
         Pacient Maria = new Pacient("Ion","Ionul","2003.04.08", "adresa", "username","parola", "teelfon","disgnostic");
